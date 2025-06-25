bad99c347e0bd2f2c419dde1e29de1c2788ffe1b
Chirag Patil <patilchirag192k@gmail.com>
2025-06-25 14:31:56 +0530

    Welcome to line comparison problem

---
+++ LineComparison.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LineCamparisonProblem
+{
+    class LineComparison
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Line Comparison Problem");
+
+        }
+    }
+}
